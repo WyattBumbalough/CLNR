67793b154f98e2a5cd3151663ebafbfe7b7fb85f
Tumbunski <tumbunski@gmail.com>
2026-09-18 20:02:58 -0500

    a
    hehe

--- src/levels/test/he.gd
+++ src/levels/test/he.gd
@@ -1,13 +1,16 @@
 extends Interactable
 
+var dude = 0
+
 func _on__looked_at():
-	if %BDR.visible == true:
+	if %BDR.visible == true and dude < 20:
 		%HUH.play()
 		print("Your ass is mine, little Bumby.")
-	else:
-		pass
-	
-
+		dude +=1
+	if %BDR.visible == true and dude >= 20:
+		print("Alright nigga, calm down.")
+		%HUH.play()
+		dude +=1
 
 func _on_huh_finished() -> void:
 	get_tree().quit()
